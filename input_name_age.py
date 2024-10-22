81f1b99721beb9d7fbde1386cb2c7025f3bf7a82
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-22 19:30:27 +0800

    Algotithm for the program

--- input_name_age.py
+++ input_name_age.py
@@ -1,1 +1,7 @@
 # Creating a program that ask user to input name and age
+
+# Print error message when input is not valid
+
+# def of valid name and age, storing collected info into array
+
+# if Yes, ask again for input. except No, show the name and age of the oldest person
