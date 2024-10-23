c66bb8e995d4fcd7ead31ef808826c18bb472d77
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-23 21:51:36 +0800

    Input name and age - working!

--- input_name_age.py
+++ input_name_age.py
@@ -1,5 +1,17 @@
 # Creating a program that ask user to input name and age
+name_age_list = {}
 
+name = input("Please input a name: ")
+age = int(input("Please input the age: "))
+
+name_age_list[name] = {
+    "age" : age
+}
+
+print("__________________________")
+print(f"Name:\t{name}")
+print(f"Age:\t{name_age_list[name]["age"]}")
+print("__________________________")
 # Print error message when input is not valid
 
 # def of valid name and age, storing collected info into array
