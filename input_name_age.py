4678ffee566331d96f943a83f6ee4302724295ee
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-25 02:20:50 +0800

    Rearranged and changed some lines

--- input_name_age.py
+++ input_name_age.py
@@ -11,16 +11,25 @@ while True:
         print(f"Age:\t{age}")
         print("__________________________")
 
-        user_inputs.append({'name': name, 'age': age})
-
-        add_more = input("Add More?(y/n): ")
-
+        user_inputs.append((name, age))
+        
+        while True:
+            add_more = input("Add More?(y/n): ")
+            
+            if add_more == 'n':
+                print("-------All Entries------")
+                for name, age in user_inputs:
+                    print(f"~ Name:{name}\tAge:{age}")
+                print("------------------------")
+                break
+            elif add_more == 'y':
+                break
+            else:
+                 print("Please choose between 'y' or 'n' only")
+                 
         if add_more == 'n':
             break
-
-        elif add_more != 'y':
-           print("Please choose between 'y' or 'n' only")
-
+        
 # Print error message when input is not valid
     except ValueError:
         print("****Invalid Input****\n --Please Try Again--")
@@ -28,5 +37,3 @@ while True:
 # def of valid name and age, storing collected info into array
 
 # if Yes, ask again for input. except No, show the name and age of the oldest person
-for inputs in user_inputs:
-    print(inputs)
