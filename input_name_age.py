f9cb8609fa54d50421e0083eda45ff8dfdf69c22
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-10-24 23:54:52 +0800

    Modified some lines, added the method of displaying list of all inputs if user decided not to add more

--- input_name_age.py
+++ input_name_age.py
@@ -1,28 +1,32 @@
 # Creating a program that ask user to input name and age
-name_age_list = {}
+user_inputs = []
 
 while True:
     try:
         name = input("Please input a name: ")
-        age = int(input("Please input the age: "))
+        age = int(input(f"Please input {name}'s age: "))
         
         print("__________________________")
         print(f"Name:\t{name}")
         print(f"Age:\t{age}")
         print("__________________________")
 
+        user_inputs.append({'name': name, 'age': age})
+
         add_more = input("Add More?(y/n): ")
 
         if add_more == 'n':
             break
-        elif add_more != 'y':
-            print("Please choose between 'y' or 'n' only")
-    except:
-        print("****Invalid Input****\n **Please Try Again**")
 
+        elif add_more != 'y':
+           print("Please choose between 'y' or 'n' only")
 
 # Print error message when input is not valid
+    except ValueError:
+        print("****Invalid Input****\n --Please Try Again--")
 
 # def of valid name and age, storing collected info into array
 
 # if Yes, ask again for input. except No, show the name and age of the oldest person
+for inputs in user_inputs:
+    print(inputs)
